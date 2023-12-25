b9ed3dffbfabd371fc3851c42b0e121903ad4add
ML <60942144+MattMattL@users.noreply.github.com>
2023-12-25 14:00:21 +0000

    Update main.m

--- main.m
+++ main.m
@@ -1,30 +1,3 @@
-%
-%	Project			Minesweeper Solver
-%	Program Name	Sweep Child O' Mine
-%	Version			lost counting
-%
-%	File Created	13/02/2019
-%	Last Modified	11/05
-%	Submission Due	13/05
-%
-%
-%	Description
-%
-%	    This GUI-based MatLab program is designed to semi-automatically solve the
-% 	game Minesweeper. When numeric and spatial information given by the user is
-% 	appropriate, with a little help from two external naive-AI algorithm libraries,
-% 	the program tries to eliminate blocks whos state could be determined.
-%
-%
-%	Linked Libraries
-%	 - main.m
-%	 - mapInit.m
-%	 - buttonInit.m
-%	 - setBlockState.m
-%	 - simpleSolver.m
-%	 - aiSolver.m
-%
-
 clear;
 clc;
 close all;
